415be32cb43ae8bdbebba0088138f59e234b3a08
Ayomide Adewumie <aoa4955@nyu.edm>
2024-06-27 11:55:50 -0400

    i was able to set a command or each prompts

--- practice_input.py
+++ practice_input.py
@@ -2,7 +2,11 @@
 A little assignment to practice receiving text input from the user in Python programming.
 Do not run this file... run main.py instead.
 """
-
+def print_with_line_break():
+  """
+  Prints out the text, 'Hello world!' with a line break at the end
+  """
+  # write your code here
 
 def get_favorite_vegetable():
     """
@@ -10,7 +14,8 @@ def get_favorite_vegetable():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite vegetable.
     """
-    # write your code here.
+    vegetable = input("Please enter your favorite vegetable: ")
+    print(f"Interesting! I also love {vegetable}!")
 
 
 def get_favorite_number():
@@ -19,7 +24,8 @@ def get_favorite_number():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite number.
     """
-    # write your code here.
+    number = input("Please enter your favorite: ")
+    print(f"Interesting! I also love {number}!")
 
 
 def get_name_and_zodiac_sign():
@@ -29,7 +35,10 @@ def get_name_and_zodiac_sign():
     Then print out, "Interesting! My name is also X, and I'm also a Y!",
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
-    # write your code here.
+    name = input("Please enter your name: ")
+    zodiac_sign = input("Please enter your zodiac sign: ")
+    print(f"Interesting! My name is also {name}, and I'm also a {zodiac sign}!")
+    
 
 
 def get_name_and_age():
@@ -39,4 +48,6 @@ def get_name_and_age():
     Then print out, "Interesting! My name is also X, and I'm also Y years old!",
     where X and Y are replaced by the user's name and age, respectively.
     """
-    # write your code here.
+    name = input("Please enter your name: ")
+    age = input("Please enter your age: ")
+    print(f"Interesting! My name is also {name}, and I'm also {age} years old!")
